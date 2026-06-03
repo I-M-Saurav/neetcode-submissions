52f05fe9f674bf124d4b210c34fc00bb7ec24c69
I-M-Saurav <sauravkumad1507@gmail.com>
2026-06-03 23:52:37 +0530

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+   public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n + 1, 1e9);
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (i + nums[i] >= j) {
+                    dp[j] = min(dp[j], 1 + dp[i]);
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+};
